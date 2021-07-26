fd2a5b416825a0718c32a3c4ae6124ed97ce9c2a
Lambdanaut <jthomas@rapidsos.com>
2021-07-26 18:11:30 -0400

    updates version to 0.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 from setuptools import setup
 
 setup(name='voicemsg',
-      version='0.1',
+      version='0.0.1',
       description='Library to record audio until a silence is encountered',
       url='http://github.com/lambdanaut/voicemsg',
       author='Lambdanaut',
